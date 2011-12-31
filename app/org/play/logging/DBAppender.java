c5925f52ee63ea7858a9baf5a3fc58af7f1bf654
Ron Gross <ron.gross@gmail.com>
2011-12-31 19:56:50 +0200

    Import code from DBAppender.

--- app/org/play/logging/DBAppender.java
+++ app/org/play/logging/DBAppender.java
@@ -4,7 +4,10 @@ import com.google.common.base.Joiner;
 import com.google.common.collect.Lists;
 import org.apache.log4j.AppenderSkeleton;
 import org.apache.log4j.spi.LoggingEvent;
+import play.db.jpa.JPA;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
 import java.lang.management.ManagementFactory;
 import java.net.UnknownHostException;
 import java.util.Date;
@@ -26,7 +29,19 @@ public class DBAppender extends AppenderSkeleton {
                 Thread.currentThread().getName(),
                 hostname
         );
-        logLine.save();
+
+        // http://stackoverflow.com/a/8689444/11236
+        // Save the log line in a separate transaction
+        EntityManager em = JPA.newEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        try {
+            tx.begin();
+            em.persist(loggingEvent);
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        }
     }
 
     private String getStackTraceStr(LoggingEvent loggingEvent) {
