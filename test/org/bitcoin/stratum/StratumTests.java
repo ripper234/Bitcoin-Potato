f8c840db400027aed891ad9597021d92df104c1b
Ron Gross <ron.gross@gmail.com>
2012-01-13 18:43:44 +0200

    Change test to prodnet address.

--- test/org/bitcoin/stratum/StratumTests.java
+++ test/org/bitcoin/stratum/StratumTests.java
@@ -5,12 +5,15 @@ import org.junit.Test;
 import play.test.UnitTest;
 
 public class StratumTests extends UnitTest {
+
+    private final String someTestnetAddress = "mw5h6BDh77GUPc4DCmzjoU7ry2R4exoaYr";
+    private final String someProdAddress = "14obksEpeUTAKsdKsw3vTSUoiYeK9S3thA";
+
     @Test
     public void sanity() {
         Stratum stratum = new Stratum(NetworkParameters.testNet(), "http://chicago.stratum.bitcoin.cz:8000/");
-        String someTestnetAddress = "mw5h6BDh77GUPc4DCmzjoU7ry2R4exoaYr";
 
-        AddressInfo addressInfo = stratum.getAddressInfo(someTestnetAddress);
+        AddressInfo addressInfo = stratum.getAddressInfo(someProdAddress);
 
         System.out.println("Foobar");
     }
