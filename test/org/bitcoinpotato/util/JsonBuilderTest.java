50e8d029ae790154d8cc73d9d83dce631461ead3
Ron Gross <ron.gross@gmail.com>
2012-01-16 09:13:04 +0200

    Minor

--- test/org/bitcoinpotato/util/JsonBuilderTest.java
+++ test/org/bitcoinpotato/util/JsonBuilderTest.java
@@ -6,6 +6,7 @@ import play.test.UnitTest;
 public class JsonBuilderTest extends UnitTest {
     @Test
     public void sanity() {
+        // http://stackoverflow.com/questions/8876271/how-to-serialize-a-jsonobject-without-too-much-quotes/8876377#8876377
         String json = new JsonBuilder()
                 .add("key1", "value1")
                 .add("key2", "value2")
